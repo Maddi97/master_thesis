7ea20b0fcc63b8cd16ef287e9beef3d56d07b5ad
Maddi97 <sgdmaddi.97@gmail.com>
2022-12-22 12:00:59 +0100

    init structure

---
+++ carsim/Assets/Scripts/CarController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    private const string HORIZONTAL = "Horizontal";
+    private const string VERTICAL = "Vertical";
+
+    private float horizontalInput;
+    private float verticalInput;
+    private float currentSteerAngle;
+    private float currentbreakForce;
+    private bool isBreaking;
+
+    [SerializeField] private float motorForce;
+    [SerializeField] private float breakForce;
+    [SerializeField] private float maxSteerAngle;
+
+    [SerializeField] private WheelCollider frontLeftWheelCollider;
+    [SerializeField] private WheelCollider frontRightWheelCollider;
+    [SerializeField] private WheelCollider rearLeftWheelCollider;
+    [SerializeField] private WheelCollider rearRightWheelCollider;
+
+    [SerializeField] private Transform frontLeftWheelTransform;
+    [SerializeField] private Transform frontRightWheeTransform;
+    [SerializeField] private Transform rearLeftWheelTransform;
+    [SerializeField] private Transform rearRightWheelTransform;
+    
+	[SerializeField] private UnityEngine.UI.Text text;
+	[SerializeField] private Transform car;
+	[SerializeField] public int count;
+    
+
+    private void FixedUpdate()
+    {
+        GetInput();
+        HandleMotor();
+        HandleSteering();
+        UpdateWheels();
+    }
+
+
+    private void GetInput()
+    {
+        horizontalInput = Input.GetAxis(HORIZONTAL);
+        verticalInput = Input.GetAxis(VERTICAL);
+        isBreaking = Input.GetKey(KeyCode.Space);
+    }
+
+    private void HandleMotor()
+    {
+        frontLeftWheelCollider.motorTorque = verticalInput * motorForce;
+        frontRightWheelCollider.motorTorque = verticalInput * motorForce;
+        currentbreakForce = isBreaking ? breakForce : 0f;
+        ApplyBreaking();       
+    }
+
+    private void ApplyBreaking()
+    {
+        frontRightWheelCollider.brakeTorque = currentbreakForce;
+        frontLeftWheelCollider.brakeTorque = currentbreakForce;
+        rearLeftWheelCollider.brakeTorque = currentbreakForce;
+        rearRightWheelCollider.brakeTorque = currentbreakForce;
+    }
+
+    private void HandleSteering()
+    {
+        currentSteerAngle = maxSteerAngle * horizontalInput;
+        frontLeftWheelCollider.steerAngle = currentSteerAngle;
+        frontRightWheelCollider.steerAngle = currentSteerAngle;
+    }
+
+    private void UpdateWheels()
+    {
+        UpdateSingleWheel(frontLeftWheelCollider, frontLeftWheelTransform);
+        UpdateSingleWheel(frontRightWheelCollider, frontRightWheeTransform);
+        UpdateSingleWheel(rearRightWheelCollider, rearRightWheelTransform);
+        UpdateSingleWheel(rearLeftWheelCollider, rearLeftWheelTransform);
+    }
+
+    private void UpdateSingleWheel(WheelCollider wheelCollider, Transform wheelTransform)
+    {
+        Vector3 pos;
+        Quaternion rot;
+        wheelCollider.GetWorldPose(out pos, out rot);
+        wheelTransform.rotation = rot;
+        wheelTransform.position = pos;
+        // Debug.Log(pos);
+        // (25, 87) , (-30, 90) (-30, (-25) , (24, -27)
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+	    if (other.tag == "red" && !(isleft(other.transform.position))){
+			
+		   count++;
+		}else if(other.tag == "blue" && isleft(other.transform.position)){
+			count++;
+		}else{
+			Debug.Log("Tag Problem:"+other.tag);
+		}
+		//text.text = b.ToString();
+	    
+    }
+    
+    private bool isleft(Vector3 position){
+        Debug.Log("is left function triggered");
+		Vector3 Dir = position - car.position;
+		Dir = Quaternion.Inverse(car.rotation) * Dir;
+		return (Dir.x>0);
+	}
+
+}
---
+++ carsim/README.md
@@ -0,0 +1,4 @@
+# CarController
+This repo houses the project for Unity for the self-driving vehicle trained by evolutionary network training.
+
+Parent repo: https://github.com/jonaskonig/BachelorNN
